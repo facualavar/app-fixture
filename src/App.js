7c340d9f6ac22b552043db1ae7b5de4af1875c29
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-11 22:17:31 -0300

    Delete initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "./components/Box/Box";
+import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar.js/Navbar";
+import Groups from "./sections/Groups/Groups";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box backgroundColor="#640F21" width="100vw" height="100vh">
+      
+    </Box>
+  )
 }
 
 export default App;
