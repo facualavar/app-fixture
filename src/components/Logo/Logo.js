54a2160c1de257290c131ce8f6bae3b7174e4ce1
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-11 22:19:14 -0300

    Add Component logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import logo from '../../images/world-cup-2022-logo.png'
+
+const Logo = () => {
+    return (
+        <img width="300" src={ logo } alt="Logo FIFA world cup 2022"/>
+    )
+}
+
+export default Logo
