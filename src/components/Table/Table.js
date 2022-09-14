527d68fb9576f49a51d09075256c61cd109a7bde
Facundo Alavar <facualavar@gmail.com>
2022-09-14 16:03:20 -0300

    Add component Table

---
+++ src/components/Table/Table.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Table = styled.table`
+    border: 1px solid #fff;
+    color: #fff;
+
+    thead {
+        background-color: #fff;
+        color: #640F21;
+    }
+
+    td {
+        text-align: center;
+    }
+`
+
+export default Table
