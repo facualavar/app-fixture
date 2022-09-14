db4d5661b7a20d57a908ab9022e03bddd874162b
Facundo Alavar <facualavar@gmail.com>
2022-09-14 13:08:56 -0300

    Comment unsubscribe() in authProvider

--- src/hooks/useProviderAuth.js
+++ src/hooks/useProviderAuth.js
@@ -39,19 +39,19 @@ const useProvideAuth = () => {
     // Because this sets state in the callback it will cause any ...
     // ... component that utilizes this hook to re-render with the ...
     // ... latest auth object.
-    useEffect(() => {
-        const unsubscribe = (user) => {
-            console.log("unsubscribe")
-            if (user) {
-                setUser(user)
-            } else {
-                setUser(false)
-            }
-        }
+    // useEffect(() => {
+    //     const unsubscribe = (user) => {
+    //         console.log("unsubscribe")
+    //         if (user) {
+    //             setUser(user)
+    //         } else {
+    //             setUser(false)
+    //         }
+    //     }
 
-        // Cleanup subscription on unmount
-        return () => unsubscribe()
-    }, [])
+    //     // Cleanup subscription on unmount
+    //     return () => unsubscribe()
+    // }, [])
 
     // Return the user object and auth methods
     return {
