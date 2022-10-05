929f2e064a8ada0756983dc0e25b9de768a5fbec
Facundo Alavar <facualavar@gmail.com>
2022-10-05 16:13:46 -0300

    Home fix view groups only authenticate

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -1,16 +1,19 @@
 import Box from "../../components/Box/Box"
 import GroupsGrid from "../../components/GroupsGrid/GroupsGrid"
 import Logo from "../../components/Logo/Logo"
+import useAuth from "../../hooks/useAuth"
 import "./home.css"
 
 const Home = () => {
+    const auth = useAuth();
+
     return (
         <Box display="flex" justifyContent="center">
             <Box className="container">
                 <Box display="flex" justifyContent="center">
                     <Logo />
                 </Box>
-                <GroupsGrid />
+                {auth.user ? <GroupsGrid /> : null}
             </Box>
         </Box>
     )
